337bd58d1da6aa4bc25a23bb0efcacf1b229cfa8
Bruno Aguirre Peña <bruno.aguirre.p1997@gmail.com>
2022-08-26 20:57:40 -0500

    chore: Inicializacion del proyecto mobile

---
+++ android/app/src/main/kotlin/app/henko/healthycareapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package app.henko.healthycareapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      home: Scaffold(
+        body: Center(
+          child: Container(
+            child: Text('Primera Interfaz Utp Desarrollo Mobile'),
+          ),
+        ),
+      ),
+    );
+  }
+}
